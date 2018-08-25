11666ab9ce4b3beea39cbb0a0e2b10dad0b604e9
Jiwoon Kim <diana0501@naver.com>
2018-08-25 11:20:46 +0000

    Added modal to channel.html

--- application.py
+++ application.py
@@ -13,9 +13,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Store channels
-channels = {}
-
 
 @app.route("/")
 def index():
@@ -37,12 +34,29 @@ def channel():
         Most recent channel and recent messages are displayed
         User may create a new channel or send a new message to all """
 
-    #
-
     # Store display name from session
     display_name = session.get("display_name")
 
-    return render_template("channel.html", display_name=display_name)
+    # Store channels from session
+    if session.get("channels") is None:
+        session["channels"] = {}
+    channels = session["channels"]
+
+    return render_template("channel.html", display_name=display_name, channels=channels)
+
+
+@app.route("/newchannel", methods=["POST"])
+def newchannel():
+    """ User has clicked button to add new channel
+        and prompted for new channel's name """
+
+    # Ensure channel name is unique in javascript
+
+    # Add new channel name to session
+
+    session["channels"]
+
+    return redirect("/channel")
 
 
 @app.route("/signin", methods=["POST"])
