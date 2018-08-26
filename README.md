44c8a677db1a0a7f88473fb9e681abf37a6a812e
Jiwoon Kim <diana0501@naver.com>
2018-08-26 07:00:43 +0000

    Fixed javascript error and adding new channel works

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# Project 2: Online Messaging Service
+# Web50 Project 2 | Web50 프로젝트 2
+> Practicing JavaScript to run single page application (to communicate with servers via Ajax and Socket IO).
 
-For CS50 Web Programming with Python and JavaScript.
+> JavaScript를
 
-Python and Flask are used as a server.
-Building and online messaging service similiar to Slack.
+## Project 2: Online Messaging Service
+- For CS50 Web Programming with Python and JavaScript.
+- Created an online messaging service similiar to Slack.
+- Used Python and Flask as the server framerwork.
+-
+
+## 프로젝트 2: 온라인 메세지 서비스
+- 하버드의 CS50 MOOC(edX)의 Web Programming with Python and JavaScript 수업의 프로젝트 2.
+- Slack과 유사한 온라인 메세지 서비스를 개발.
+- Python과 Flask를 서버 프레임워크로 사용함.
+
+
+## what I learned | 무엇을 배웠는가
+>
--- static/21.js
+++ static/21.js
@@ -1,29 +1,25 @@
 // when new channel button is clicked, create new channel
-document.querySelector("#create-newchannel").addEventListener('clicked', createNewChannel);
+document.querySelector("#create-newchannel").addEventListener('click', createNewChannel);
 
 // function for new channel button clicked
 function createNewChannel() {
 
     // get input for new channel name
     let channel_input = document.querySelector("#newname");
-    console.log(channel_input);
     let new_name = channel_input.value;
-    console.log(new_name);
 
     // ensure input for new channel name is submitted
     if (new_name.length === 0) {
-        console.log("not submitted");
         let error = document.createElement('p');
         error.textContent = "Must enter new channel name";
         error.style.color = "red";
-        document.querySelector(".modal-body").append(error);
+        document.querySelector(".modal-body").appendChild(error);
         channel_input.value = "";
     }
     else {
         // check if name already exists
 
         // create form to send to server via post
-        console.log("sending to server");
         let form = document.createElement("form");
         form.setAttribute("method", "post");
         form.setAttribute("action", "/newchannel");
@@ -33,7 +29,7 @@ function createNewChannel() {
         input.setAttribute("type", "hidden");
         input.setAttribute("name", "channel-name");
         input.setAttribute("value", new_name);
-        form.appendChile(input)
+        form.appendChild(input)
 
         // send form to server
         document.body.appendChild(form);
